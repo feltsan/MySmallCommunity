d016160edf871eb8ef17ad6f3173490fab8a7621
feltsan <ivan.feltsan@gmail.com>
2015-11-17 17:12:38 +0200

    add parse

---
+++ app/src/main/java/com/thinkmobiles/mysmallcommunity/App.java
@@ -0,0 +1,47 @@
+package com.thinkmobiles.mysmallcommunity;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+/**
+ * Created by feltsan on 17.11.15.
+ */
+public class App extends Application {
+    public static final String PARSE_ID = "njNebBBm6isbJ7zTwaW2tVL1QFuF7jp5gBxzUeDt";
+    public static final String PARSE_KEY = "dxSkAZBOBhd72k6NTNFfnV5ySjFPKRVsQcfcvjfF";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initParse();
+    }
+
+    private void initParse(){
+        Parse.initialize(this, App.PARSE_ID, App.PARSE_KEY);
+        ParseFacebookUtils.initialize(this);
+//        ParseUser user = new ParseUser();
+//        user.setUsername("njoy");
+//        user.setPassword("123");
+//        user.setEmail("email@example.com");
+//
+//// other fields can be set just like with ParseObject
+//        user.put("phone", "650-555-0000");
+//
+//        user.signUpInBackground(new SignUpCallback() {
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    // Hooray! Let them use the app now.
+//                } else {
+//                    // Sign up didn't succeed. Look at the ParseException
+//                    // to figure out what went wrong
+//                }
+//            }
+//        });
+
+    }
+}
--- app/src/main/java/com/thinkmobiles/mysmallcommunity/ui/activities/MainActivity.java
+++ app/src/main/java/com/thinkmobiles/mysmallcommunity/ui/activities/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.thinkmobiles.mysmallcommunity.App;
 import com.thinkmobiles.mysmallcommunity.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,29 +25,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
